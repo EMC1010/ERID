4c872b23c03e0a6bf4b6c70ff868bdbb2a7fee70
Emilio Manuel Cebu <52725264+EMC1010@users.noreply.github.com>
2023-06-05 19:23:42 +0800

    E.R._ID

--- app/src/main/java/com/cebu/erid/LandingPageActivity.kt
+++ app/src/main/java/com/cebu/erid/LandingPageActivity.kt
@@ -3,7 +3,7 @@ package com.cebu.erid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LandingPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
