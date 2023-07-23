7e687478eec3c85556e956ff50615e30192d3682
Emilio Manuel Cebu <52725264+EMC1010@users.noreply.github.com>
2023-07-23 15:09:28 +0800

    update

--- app/src/main/java/com/cebu/erid/LoginActivity.kt
+++ app/src/main/java/com/cebu/erid/LoginActivity.kt
@@ -1,12 +1,43 @@
 package com.cebu.erid
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activty_login)
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.Login)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            //Replace "your_username" and "your_password" with the correct login credentials
+            val correctUsername = "your username"
+            val correctPassword = "your password"
+
+            if (username == correctUsername && correctPassword == correctPassword) {
+                //Login Successful
+                showToast("Login Successful!")
+            } else {
+                //Login Failed
+                showToast("Invalid credentials. Please try again.")
+                }
+        }
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/cebu/erid/PatientInfoActivity.kt
+++ app/src/main/java/com/cebu/erid/PatientInfoActivity.kt
@@ -1,11 +1,20 @@
 package com.cebu.erid
 
+import android.R
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+
 
 class PatientInfoActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_patient)
+        setContentView(R.layout.activity_list_item)
+
+        val dropdown = findViewById<Spinner>(R.id.)
+        val gender = arrayOf("Male", "Female", "Gay", "Lesbian", "Transgender")
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_dropdown_item, gender)
+        dropdown.adapter = adapter
     }
 }
