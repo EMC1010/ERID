3e82579dd61927388a50dc3ffb76dacc00ef797e
Emilio Manuel Cebu <52725264+EMC1010@users.noreply.github.com>
2023-07-23 22:51:16 +0800

    update

--- app/src/main/java/com/cebu/erid/LandingPageActivity.kt
+++ app/src/main/java/com/cebu/erid/LandingPageActivity.kt
@@ -1,11 +1,27 @@
 package com.cebu.erid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputBinding
+import androidx.activity.result.contract.ActivityResultContracts
+
 
 class LandingPageActivity : AppCompatActivity() {
+
+    private lateinit var binding: LandingPageActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landingpage)
+        binding = LandingPageActivity.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.btnSignUp.setOnClickListener{
+            Log.d("btnSignUp","click")
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/cebu/erid/sample.kt
+++
@@ -1,8 +0,0 @@
-package com.cebu.erid
-
-class sample {
-    Spinner dropdown = findViewById(R.id.spinner1);
-    String[] items = new String[]{"1", "2", "3"};
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-    dropdown.setAdapter(adapter);
-}
