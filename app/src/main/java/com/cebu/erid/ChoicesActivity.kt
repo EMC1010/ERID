c0430aa365e18d9bcc2cdd29e25b80fa56110265
Emilio Manuel Cebu <52725264+EMC1010@users.noreply.github.com>
2023-07-25 01:26:45 +0800

    update

--- app/src/main/java/com/cebu/erid/ChoicesActivity.kt
+++ app/src/main/java/com/cebu/erid/ChoicesActivity.kt
@@ -7,5 +7,15 @@ class ChoicesActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choices)
+
+        fun onImageViewClick(view: View){
+            val intent = Intent(this, PatientInfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        fun onImageViewClick(view: View){
+            val intent = Intent(this,DrInfoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/cebu/erid/DrQRCodeActivity.kt
+++ app/src/main/java/com/cebu/erid/DrQRCodeActivity.kt
@@ -1,11 +1,36 @@
 package com.cebu.erid
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DrQRCodeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drqrcode)
+
+        generateandDisplayQRCode()
+    }
+
+    private fun generateandDisplayQRCode() {
+        GlobalScope.launch(Dispatchers.IO) {
+            val content = "Your_content_to_encode"
+
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap: Bitmap = barcodeEncoder.encodeBitmap(content, BarcodeFormat.QR_CODE, 300, 300)
+
+            withContext(Dispatchers.Main) {
+                val imageViewQRCode: ImageView = findViewById(R.id.imageViewQRCode)
+                imageViewQRCode.setImageBitmap(bitmap)
+            }
+        }
     }
 }
