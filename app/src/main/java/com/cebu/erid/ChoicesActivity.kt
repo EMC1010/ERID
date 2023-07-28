f8b5ff0615a4d1b7a34ad9203c4222ecb8c7a635
Emilio Manuel Cebu <52725264+EMC1010@users.noreply.github.com>
2023-07-29 00:27:00 +0800

    update

--- app/src/main/java/com/cebu/erid/ChoicesActivity.kt
+++ app/src/main/java/com/cebu/erid/ChoicesActivity.kt
@@ -2,24 +2,38 @@ package com.cebu.erid
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.cebu.erid.databinding.ActivityChoicesBinding
 
 class ChoicesActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityChoicesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choices)
+        binding = ActivityChoicesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnDR.setOnClickListener{
+            Log.d("btnDR", "click")
+            startActivity(Intent(this, DrInfoActivity::class.java))
+        }
 
-    }
-//base on the Landing Page
-    fun onImageViewClick2(view: View){
-        val intent = Intent(this, PatientInfoActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun onImageViewClick(view: View){
-        val intent = Intent(this,DrInfoActivity::class.java)
-        startActivity(intent)
+        binding.btnPatient.setOnClickListener {
+            Log.d("btnPatient", "click")
+            startActivity(Intent(this, PatientInfoActivity::class.java))
+        }
     }
 }
+//base on the Landing Page
+//    fun onImageViewClick2(view: View){
+//        val intent = Intent(this, PatientInfoActivity::class.java)
+//        startActivity(intent)
+//    }
+//
+//    fun onImageViewClick(view: View){
+//        val intent = Intent(this,DrInfoActivity::class.java)
+//        startActivity(intent)
+//    }
+//}
--- app/src/main/java/com/cebu/erid/SignUpActivity.kt
+++ app/src/main/java/com/cebu/erid/SignUpActivity.kt
@@ -1,65 +1,79 @@
 package com.cebu.erid
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import java.util.Scanner
 
 class SignUpActivity : AppCompatActivity() {
 
-    data class User(val username: String, val password: String, val firstname: String, val lastname: String)
+    data class User(
+        val username: String,
+        val password: String,
+        val firstname: String,
+        val lastname: String
+    )
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-    }
 
+        val backButton: ImageView = findViewById(R.id.btnBack)
+        backButton.setOnClickListener{
+            finish()
+        }
+    }
+}
     //1. Add the viewBinding
     //2. Get the text from editText, Username.getText
     //3. Boolean checkBox, checkBox.isChecked == true or false
     //4. Add onclicklistener in buttons
     //5. Add function for the button, sending it to the database
 
-    fun getText() {
-
-    }
-
-
-//        fun main() {
-//            val scanner = Scanner(System."in")
+//    fun getText() {
 //
-//            println("Welcome to E.R. ID")
+//    }
 //
-//            val username = getUsername(scanner)
-//            val password = getPassword(scanner)
-//            val firstname = getFirstName(scanner)
-//            val lastname = getLastName(scanner)
 //
-//            val newUser = User(username, password, firstname, lastname)
+////        fun main() {
+////            val scanner = Scanner(System."in")
+////
+////            println("Welcome to E.R. ID")
+////
+////            val username = getUsername(scanner)
+////            val password = getPassword(scanner)
+////            val firstname = getFirstName(scanner)
+////            val lastname = getLastName(scanner)
+////
+////            val newUser = User(username, password, firstname, lastname)
+////
+////            println("Sign Up Successful!")
+////            println("Your Information")
+////            println("Username: ${newUser.username}")
+////            println("Password: ${newUser.password}")
+////            println("First Name: ${newUser.firstname}")
+////            println("Surname: ${newUser.lastname}")
+////        }
 //
-//            println("Sign Up Successful!")
-//            println("Your Information")
-//            println("Username: ${newUser.username}")
-//            println("Password: ${newUser.password}")
-//            println("First Name: ${newUser.firstname}")
-//            println("Surname: ${newUser.lastname}")
+//        fun getUsername(scanner: Scanner): String {
+//            print("Username: ")
+//            return scanner.nextLine()
 //        }
-
-        fun getUsername(scanner: Scanner): String {
-            print("Username: ")
-            return scanner.nextLine()
-        }
-
-        fun getPassword(scanner: Scanner): String{
-            print("Password: ")
-            return scanner.nextLine()
-        }
-
-        fun getFirstName(scanner: Scanner): String{
-            print("First Name: ")
-            return scanner.nextLine()
-        }
-
-        fun getLastName(scanner: Scanner): String{
-            print("Surname: ")
-            return scanner.nextLine()
-        }
-}
+//
+//        fun getPassword(scanner: Scanner): String{
+//            print("Password: ")
+//            return scanner.nextLine()
+//        }
+//
+//        fun getFirstName(scanner: Scanner): String{
+//            print("First Name: ")
+//            return scanner.nextLine()
+//        }
+//
+//        fun getLastName(scanner: Scanner): String{
+//            print("Surname: ")
+//            return scanner.nextLine()
+//        }
+//}
